eaae477909231a7a51956790466080f01e80b907
jjs <jiyan.schneider@gmail.com>
2022-02-23 17:01:56 +0900

    Don't need these testing scripts anymore

--- scripts/fastai_audio.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env python3
-from fastai.data.all import *
-from fastai.vision.all import *
-from Transformers import Wav2Vec2Processor
--- scripts/fastaitest.py
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env python3
-from fastai.vision.all import *
-from fastai.data.all import *
