fef47fe2652426c7fffae6afed698a4d59274baf
jjs <jiyan.schneider@gmail.com>
2022-03-16 13:53:27 +0900

    There is only a single password

--- scripts/zip2tar.py
+++ scripts/zip2tar.py
@@ -5,11 +5,12 @@ from zipfile import ZipFile
 import tarfile
 import time
 from pathlib import Path
-from fastai.data.all import get_files,progress_bar
-from local import PASSWORDS
+from fastai.data.all import get_files, progress_bar
+from local import PASSWORD
 
 SHARE_FOLDER_ROOT= Path("/path/to/input/folder")
 SHARE_FOLDER_OUT = Path("/path/to/output/folder")
+
 
 def zip2tar(inpf, outf):
     with ZipFile(inpf) as zipf:
@@ -18,22 +19,14 @@ def zip2tar(inpf, outf):
             for zip_info in progress_bar(zipf.infolist()):
                 tar_info = tarfile.TarInfo(name=zip_info.filename)
                 tar_info.size = zip_info.file_size
-                zip_info
                 try:
                     fileobj = zipf.open(zip_info.filename)
-                except Exception as e:
-                    if pw is None:
-                        for key in PASSWORDS.keys():
-                            if key in str(inpf):
-                                pw = PASSWORDS[key]
-                        if pw is None:
-                            pw = input("Please input password\n")
-                    fileobj = zipf.open(zip_info.filename, pwd=bytes(pw, encoding="utf-8"))
+                except Exception as _:
+                    fileobj = zipf.open(
+                        zip_info.filename, pwd=bytes(PASSWORD, encoding="utf-8")
+                    )
+                tarf.addfile(tarinfo=tar_info, fileobj=fileobj)
 
-                tarf.addfile(
-                    tarinfo=tar_info,
-                    fileobj=fileobj
-                )
 
 zip_files = get_files(SHARE_FOLDER_ROOT, extensions=[".zip"])
 
