35646c586518b86bb62018bf0315dbbe0c801a29
jjs <jiyan.schneider@gmail.com>
2022-03-16 14:01:45 +0900

    rename itps corpus file

--- dsets/itps_corpus.py
+++ dsets/itps_corpus.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 from ctypes import ArgumentError
-from fastai.data.all import untar_data
+from fastai.data.all import untar_data, Path, get_files
 import pandas as pd
 
 ANNOTATION_DATA_URL = "https://www.dropbox.com/s/92tbhcdlymk5s0w/annotation_data.tar.gz?dl=1"
@@ -9,11 +9,10 @@ def get_annotation_data(lang=None):
     p = untar_data(ANNOTATION_DATA_URL)
     p = p / "アノテーションデータ"/ "20210511_アノテーションデータ_full"
     df = pd.read_csv(p / "annotation_data.csv", index_col=0)
-    print(df)
     df["filename"] = df["audio_filename"].apply(lambda x: p / x)
     df["lang"] = df["audio_filename"].apply(lambda x: x.split("/")[1])
     if lang is None:
-        return df
+        return p, df
     elif lang =="en":
         return p, df[df["lang"] == lang].reset_index(drop=True)
     elif lang == "jp":
