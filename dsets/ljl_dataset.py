c62c9724685ee7f6e4634e893a8cde2d1dc28773
jjs <jiyan.schneider@gmail.com>
2022-03-16 17:49:47 +0900

    change name for ljl datset

--- dsets/ljl_dataset.py
+++ dsets/ljl_dataset.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 from fastai.data.all import untar_data
 import pandas as pd
+from .dset_config import DatasetConfig
 
 LJ_SPEECH_URL = "https://www.dropbox.com/s/cwq264n040guqhj/LJSpeech-1.1.tar.bz2?dl=1"
 
-def _get_ljl_data():
+def get_ljl_data(_):
     p = untar_data(LJ_SPEECH_URL)
     df = pd.read_csv(p / "metadata.csv", sep="|", header=None)
     df[0] = df[0].apply(lambda x: p / "wavs" / (x + ".wav"))
