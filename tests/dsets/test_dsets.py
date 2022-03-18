152f40d3ca73e331862e3d0cb309758010a7730a
jjs <jiyan.schneider@gmail.com>
2022-03-18 15:46:03 +0900

    Add unit tests for different datsets

--- tests/dsets/test_dsets.py
+++ tests/dsets/test_dsets.py
@@ -5,6 +5,55 @@ import os
 
 from dsets.librispeech import LIBRISPEECH_DSETS
 
+
+def dset_ok(df):
+    has_filenames = "filename" in df.columns
+    has_texts = "text" in df.columns
+    has_all_files = df["filename"].apply(os.path.exists).all()
+    return has_filenames and has_texts and has_all_files
+
+
+def test_get_ENGLISH_DATASETS():
+    p, df = get_datasets(ENGLISH_DATASETS)
+    assert len(df["original_dset"].unique()) == 3
+    assert set(df["original_dset"].unique()) == {"itps", "librispeech", "ljl"}
+    assert dset_ok(df)
+
+
+def test_get_JAPANESE_DATASETS():
+    p, df = get_datasets(JAPANESE_DATASETS)
+    assert len(df["original_dset"].unique()) == 2
+    assert set(df["original_dset"].unique()) == {"itps", "jsut"}
+    assert dset_ok(df)
+
+
+def test_get_itps_en():
+    p, df = get_datasets(
+        [DatasetConfig(name="itps", lang="en", split="both")],
+    )
+    assert len(p) == 1
+    assert df.shape[0] == 2807
+    assert dset_ok(df)
+
+
+def test_get_itps_jp():
+    p, df = get_datasets(
+        [DatasetConfig(name="itps", lang="jp", split="both")],
+    )
+    assert len(p) == 1
+    assert df.shape[0] == 3341
+    assert dset_ok(df)
+
+
+def test_get_itps_both():
+    p, df = get_datasets(
+        [DatasetConfig(name="itps", lang="both", split="both")],
+    )
+    assert len(p) == 1
+    assert df.shape[0] == 6148
+    assert dset_ok(df)
+
+
 def test_get_itps_librispeech():
     p, df = get_datasets(
         [
@@ -14,16 +63,10 @@ def test_get_itps_librispeech():
     )
     assert len(p) == 2
     assert df.shape[0] == 9012
-    assert df["filename"].apply(os.path.exists).all()
+    assert dset_ok(df)
 
-def test_get_ENGLISH_DATASETS():
-    p, df = get_datasets(ENGLISH_DATASETS)
-    assert df["filename"].apply(os.path.exists).all()
-    assert len(df["original_dset"].unique()) == 3
-    assert set(df["original_dset"].unique()) == {"itps", "librispeech", "ljl"}
 
-def test_get_JAPANESE_DATASETS():
-    p, df = get_datasets(JAPANESE_DATASETS)
-    assert df["filename"].apply(os.path.exists).all()
-    assert len(df["original_dset"].unique()) == 2
-    assert set(df["original_dset"].unique()) == {"itps", "jsut"}
+def test_get_ljspeech():
+    p, df = get_datasets([DatasetConfig(name="ljl", split="both")])
+    assert df.shape == (13100, 5)
+    assert dset_ok(df)
