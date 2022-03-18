3d7f4a03bdfaf4919006dd828bb57d20cbbbc120
jjs <jiyan.schneider@gmail.com>
2022-03-18 19:42:57 +0900

    added a helper function for train test split

--- scripts/data_prep/helpers.py
+++ scripts/data_prep/helpers.py
@@ -1,6 +1,19 @@
 #!/usr/bin/env python3
-import tarfile, os
+import os
+import tarfile
+from pathlib import Path
+
+import pandas as pd
+from fastai.data.transforms import RandomSplitter
+
 
 def make_tarfile(output_filename, source_dir):
     with tarfile.open(output_filename, "w:gz") as tar:
         tar.add(source_dir, arcname=os.path.basename(source_dir))
+
+def train_test_split(df: pd.DataFrame) -> pd.DataFrame:
+    splits = RandomSplitter(seed=42, valid_pct=0.2)(df)
+    df["filename"] = df["filename"].apply(Path)
+    df["test"] = False
+    df.loc[splits[1], "test"] = True
+    return df
