a6cabad117e4a36fc7ca5c2c8cf3c5b99762d40e
jjs <jiyan.schneider@gmail.com>
2022-03-18 15:46:12 +0900

    use prepared LJ speech dataset

--- dsets/ljl_dataset.py
+++ dsets/ljl_dataset.py
@@ -3,11 +3,14 @@ from fastai.data.all import untar_data
 import pandas as pd
 from .dset_config import DatasetConfig
 
-LJ_SPEECH_URL = "https://www.dropbox.com/s/cwq264n040guqhj/LJSpeech-1.1.tar.bz2?dl=1"
+LJ_SPEECH_URL = "https://www.dropbox.com/s/h0d8fa13ylwpssq/LJSpeech-1.1.tar.gz?dl=1"
 
-def get_ljl_data(_):
+def get_ljl_data(dset_config: DatasetConfig):
     p = untar_data(LJ_SPEECH_URL)
-    df = pd.read_csv(p / "metadata.csv", sep="|", header=None)
-    df[0] = df[0].apply(lambda x: p / "wavs" / (x + ".wav"))
-    df = df.rename({0: "filename", 1: "text (numbers)", 2: "text"}, axis="columns")
+    df = pd.read_csv(p / "metadata.csv", index_col=0)
+    if not isinstance(df, pd.DataFrame):
+        raise TypeError("Couldn't read dataframe correctly")
+    if dset_config.split == "train": df = df[~df["test"]].copy()
+    elif dset_config.split == "test": df = df[df["test"]].copy()
+
     return p, df
