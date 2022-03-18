6ba3f8e17f78b937d90cc1d913bfb9da66664838
jjs <jiyan.schneider@gmail.com>
2022-03-18 16:21:38 +0900

    Updating jsut dataset

--- dsets/jsut_dataset.py
+++ dsets/jsut_dataset.py
@@ -1,36 +1,15 @@
 #!/usr/bin/env python3
-from fastai.data.all import untar_data, get_files
+from fastai.data.all import untar_data
 import pandas as pd
+from .dset_config import DatasetConfig
 
-JSUT_URL = "https://www.dropbox.com/s/a2z6bklpaphiu1k/jsut_ver1.1.zip?dl=1"
+JSUT_URL = "https://www.dropbox.com/s/o949otj06b9ucmm/jsut_ver1.1.tar.gz?dl=1"
 
-def _get_info_file(fn):
-    out_dict = {}
-    with open(fn, "r") as f:
-        for line in f:
-            line = line.split(":")
-            filename = str(fn.parent / "wav" / (line[0] + ".wav"))
-            label = ":".join(line[1:]).strip()
-            out_dict[filename] = label
-        return out_dict
-
-
-def _get_info(text_files, kind="transcript_utf8", colname="text"):
-    d = {}
-    for f in text_files:
-        if f.stem == kind:
-            d.update(_get_info_file(f))
-    return (
-        pd.DataFrame(pd.Series(d))
-        .reset_index()
-        .rename({"index": "filename", 0: colname}, axis="columns")
-    )
-
-
-def get_jsut_data(dataconfig):
-    p = untar_data(JSUT_URL)
-    text_files = get_files(p, extensions=[".txt"])
-    d = _get_info(text_files, kind="transcript_utf8", colname="text")
-    df = _get_info(text_files, kind="recording_info", colname="recording_info")
-    d = pd.merge(d, df, on="filename", how="inner")
-    return p, d
+def get_jsut_data(dset_config: DatasetConfig):
+    p = untar_data(JSUT_URL, force_download=True)
+    df = pd.read_csv(p / "metadata.csv", index_col=0)
+    if not isinstance(df, pd.DataFrame):
+        raise TypeError("Couldn't read dataframe correctly")
+    if dset_config.split == "train": df = df[~df["test"]].copy()
+    elif dset_config.split == "test": df = df[df["test"]].copy()
+    return p, df
