401299f86f88f2a3c569e5574fb2ef57e77d85bf
jjs <jiyan.schneider@gmail.com>
2022-03-18 19:54:13 +0900

    add spreads url + fix language column name

--- dsets/simple_dsets.py
+++ dsets/simple_dsets.py
@@ -6,6 +6,7 @@ from .dset_config import DatasetConfig
 
 LJ_SPEECH_URL = "https://www.dropbox.com/s/h0d8fa13ylwpssq/LJSpeech-1.1.tar.gz?dl=1"
 JSUT_URL = "https://www.dropbox.com/s/o949otj06b9ucmm/jsut_ver1.1.tar.gz?dl=1"
+SPREADS_URL = "https://www.dropbox.com/s/7325sa83zdgl3le/NICT_SPREDS.tar.gz?dl=1"
 
 def get_ljl_data(dset_config: DatasetConfig, force_download=False):
     p = untar_data(LJ_SPEECH_URL, force_download=force_download)
@@ -30,13 +31,10 @@ def get_nictspreads_data(dset_config: DatasetConfig, force_download=False):
 
     p = untar_data(SPREADS_URL, force_download=force_download)
     df = pd.read_csv(p / "metadata.csv", index_col=0)
-    langs = set(df["language"].unique())
     if not isinstance(df, pd.DataFrame):
         raise TypeError("Couldn't read dataframe correctly")
     if dset_config.split == "train": df = df[~df["test"]].copy()
     elif dset_config.split == "test": df = df[df["test"]].copy()
-    if dset_config.lang == "all":
-        pass
-    else:
-        df = df[df["lang"] == dset_config.lang].copy()
+    if dset_config.lang == "all": pass
+    else: df = df[df["language"] == dset_config.lang].copy()
     return p, df
