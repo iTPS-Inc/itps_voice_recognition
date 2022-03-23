97ffc36d0443de662bd8a1703b37a56159ff7ff8
jjs <jiyan.schneider@gmail.com>
2022-03-23 16:04:53 +0900

    remove unused plot_waveform function

--- itpsaudio/core.py
+++ itpsaudio/core.py
@@ -39,7 +39,7 @@ def plot_waveform(waveform, sample_rate, title="Waveform", xlim=None, ylim=None)
     plt.show(block=False)
 
 
-def plot_specgram(waveform, sample_rate, title="Spectrogram", xlim=None):
+def plot_specgram(waveform, sample_rate, title="Spectrogram", xlim=None, ctx=None):
     waveform = waveform.numpy()
     num_channels, _ = waveform.shape
     figure, axes = plt.subplots(num_channels, 1)
@@ -214,25 +214,6 @@ def get_sine_sweep(sample_rate, offset=DEFAULT_OFFSET):
     signal = torch.sin(cummulative).unsqueeze(dim=0)
     return signal
 
-
-def plot_waveform(waveform, sample_rate, title="Waveform", xlim=None, ylim=None):
-    waveform = waveform.numpy()
-    num_channels, num_frames = waveform.shape
-    time_axis = torch.arange(0, num_frames) / sample_rate
-    figure, axes = plt.subplots(num_channels, 1)
-    if num_channels == 1:
-        axes = [axes]
-    for c in range(num_channels):
-        axes[c].plot(time_axis, waveform[c], linewidth=1)
-        axes[c].grid(True)
-        if num_channels > 1:
-            axes[c].set_ylabel(f"Channel {c+1}")
-        if xlim:
-            axes[c].set_xlim(xlim)
-        if ylim:
-            axes[c].set_ylim(ylim)
-    figure.suptitle(title)
-    plt.show(block=False)
 
 def show_waveform(im, ax=None, figsize=None, title=None, ctx=None, **kwargs):
     "Show a spectogram or PyTorch image on `ax`."
