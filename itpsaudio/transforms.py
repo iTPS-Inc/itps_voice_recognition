ed9d858121ece482858ebc95d9364ef82134d097
jjs <jiyan.schneider@gmail.com>
2022-05-20 14:26:19 +0900

    add japanese tokenizer class

--- itpsaudio/transforms.py
+++ itpsaudio/transforms.py
@@ -34,6 +34,39 @@ class TargetProcessor(Transform):
         with self.proc.as_target_processor():
             return self.proc.decode(y)
 
+class JPTransformersTokenizer(Transform):
+    hira = "あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほまみむめもやゆよらりるれろわをんがぎぐげござじずぜぞだじづでどばびぶべぼぱぴぷぺぽゃゅょっ "
+    kata = "アイウエオカキクケコサシスセソタチツテトナニヌネノハヒフヘホマミムメモヤユヨラリルレロワヲンガギグゲゴザジズゼゾダジヅデドバビブベボパピプペポャュョッ "
+    trans=str.maketrans(kata, hira)
+    node_format_csv = r"%f[7]|"
+    eos_format_csv  = r"[EOS]\n"
+    unk_format_csv = r"%m|"
+
+    def __init__(self, tok=None,mcb=None):
+      self.tokenizer = tok
+      self.mcb = mcb
+
+    def kata2hira(self, s):
+        return s.translate(self.trans).strip()
+
+    def mecab_step(self, s:str):
+      s = self.mcb.parse(s.lower())
+      return "[BOS]"+ self.kata2hira(s)
+
+    def encodes(self, s: str)-> TensorText:
+        s = self.mecab_step(s)
+        toks = tensor(self.tokenizer(s)["input_ids"])
+        return TensorText(toks)
+
+    def batch_decode(self, xs):
+        if len(xs.shape) == 2:
+          no_pads = [x[x != self.tokenizer.pad_token_id] for x in xs]
+          decoded = [self.tokenizer.decode(x) for x in no_pads]
+          return decoded
+        raise AttributeError
+
+    def decodes(self, x):
+        return TitledStr(self.tokenizer.decode(x.cpu().numpy()))
 
 class TransformersTokenizer(Transform):
     def __init__(self, tokenizer):
