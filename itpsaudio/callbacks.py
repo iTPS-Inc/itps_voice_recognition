a562b114087497ab631fdd68d99bb0a58d228800
jjs <jiyan.schneider@gmail.com>
2022-05-29 12:58:56 +0900

    Formatting

--- itpsaudio/callbacks.py
+++ itpsaudio/callbacks.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
-from fastai.callback.all import MixedPrecision
-from fastai.data.all import to_float, store_attr
-from fastai.callback.tensorboard import TensorBoardBaseCallback
-import torch
 import neptune
-from  neptune.new.types import File
 import numpy as np
+import torch
+from fastai.callback.all import MixedPrecision
+from fastai.callback.tensorboard import TensorBoardBaseCallback
+from fastai.data.all import store_attr, to_float
 
 
 class MixedPrecisionTransformers(MixedPrecision):
@@ -13,46 +12,57 @@ class MixedPrecisionTransformers(MixedPrecision):
         if self.pred.logits.dtype == torch.float16:
             self.learn.pred.logits = to_float(self.pred.logits)
 
+
 class SeePreds(TensorBoardBaseCallback):
-    def __init__(self,base_model_name,tok, n_iters=50, log_dir=None, neptune=False, n_vals=6):
-      super().__init__()
-      store_attr()
+    def __init__(
+        self, base_model_name, tok, n_iters=50, log_dir=None, neptune=False, n_vals=6
+    ):
+        super().__init__()
+        store_attr()
 
     def before_fit(self):
-       self._setup_writer()
-       if self.neptune:
-         self.experiment = neptune.get_experiment()
+        self._setup_writer()
+        if self.neptune:
+            self.experiment = neptune.get_experiment()
 
     def get_valid_preds(self):
-      decoded_preds = []
-      decoded_ys = []
-      for i, (x, y) in enumerate(iter(self.dls.valid)):
-        if i < self.n_vals:
-          with torch.no_grad():
-             preds = np.argmax(self.model(x).logits.detach().cpu(), axis=-1)
-          decoded_preds += self.tok.batch_decode(preds)
-          decoded_ys += self.tok.batch_decode(y)
-        else:
-          break
-      return decoded_preds, decoded_ys
+        decoded_preds = []
+        decoded_ys = []
+        for i, (x, y) in enumerate(iter(self.dls.valid)):
+            if i < self.n_vals:
+                with torch.no_grad():
+                    preds = np.argmax(self.model(x).logits.detach().cpu(), axis=-1)
+                decoded_preds += self.tok.batch_decode(preds)
+                decoded_ys += self.tok.batch_decode(y)
+            else:
+                break
+        return decoded_preds, decoded_ys
 
     def _log_preds_to_path(self, p, y, path):
-      self.writer.add_text(f"{self.base_model_name}/prediction/{path}",f"Targets:{y}\nPredictions:{p}",  self.iter)
-      if self.neptune:
-         self.experiment.log_text(f"text_predictions/{path}", f"{self.iter}: Targ: {y}")
-         self.experiment.log_text(f"text_predictions/{path}", f"{self.iter}: Pred: {p}")
+        self.writer.add_text(
+            f"{self.base_model_name}/prediction/{path}",
+            f"Targets:{y}\nPredictions:{p}",
+            self.iter,
+        )
+        if self.neptune:
+            self.experiment.log_text(
+                f"text_predictions/{path}", f"{self.iter}: Targ: {y}"
+            )
+            self.experiment.log_text(
+                f"text_predictions/{path}", f"{self.iter}: Pred: {p}"
+            )
 
     def after_pred(self):
         if self.iter % self.n_iters == 0:
             preds = np.argmax(self.pred.logits.detach().cpu(), axis=-1)
             decoded_preds = self.tok.batch_decode(preds)
             decoded_ys = self.tok.batch_decode(self.yb[0])
-            dec_preds_valid,  dec_ys_valid = self.get_valid_preds()
+            dec_preds_valid, dec_ys_valid = self.get_valid_preds()
             for x in self.xb[0]:
-              self.writer.add_audio(f"{self.iter}: Audio", x, self.iter, 16000)
+                self.writer.add_audio(f"{self.iter}: Audio", x, self.iter, 16000)
 
-            for p,y in zip(decoded_preds, decoded_ys):
-              self._log_preds_to_path(p,y, "random")
+            for p, y in zip(decoded_preds, decoded_ys):
+                self._log_preds_to_path(p, y, "random")
 
-            for i, (p,y) in enumerate(zip(dec_preds_valid, dec_ys_valid)):
-              self._log_preds_to_path(p,y, f"valid_{i}")
+            for i, (p, y) in enumerate(zip(dec_preds_valid, dec_ys_valid)):
+                self._log_preds_to_path(p, y, f"valid_{i}")
