78f27613bba04be5e4638dcb908ee64c96c4b8df
jjs <jiyan.schneider@gmail.com>
2022-05-29 12:28:33 +0900

    remove unneeded test

--- tests/itpsaudio/test_aug_transforms.py
+++ tests/itpsaudio/test_aug_transforms.py
@@ -19,12 +19,3 @@ def _assert_equal_tensor(a, b):
 
 def _assert_nequal_tensor(a, b):
     assert not (a == b).all()
-
-def test_AudioBatchTransform():
-    xy_1 = TensorAudio([[1, 1, 1]], sr=16000).float()
-    noise, sr = torchaudio.load("noise_sample.wav")
-    noise = TensorAudio(noise, sr=sr)
-    noise_t = AddNoise(range(1, 10), noise, p=1)
-
-    x = noise_t(xy_1)
-    _assert_nequal_tensor(x, xy_1)
