88cbc55541770a6162b45c39f43862a58de134cb
jjs <jiyan.schneider@gmail.com>
2022-06-20 13:32:19 +0900

    Change WER and CER metric calculatiosn

--- itpsaudio/metrics.py
+++ itpsaudio/metrics.py
@@ -4,28 +4,62 @@ import jiwer
 from fastai.metrics import Metric
 
 class WER(Metric):
-    def __init__(self, tok): self.tok = tok
+  def __init__(self, tok):
+      self.metric_name = "wer"
+      self.tok = tok
+      self.pred_strs, self.label_strs = [], []
 
-    @property
-    def name(self): return "wer"
+  @property
+  def name(self):
+      return "wer"
 
-    def __call__(self, pred, labels):
-        pred_logits = pred.logits
-        pred_ids = np.argmax(pred_logits.detach().cpu().numpy(), axis=-1)
-        pred_str = self.tok.batch_decode(pred_ids, group_tokens=True, skip_special_tokens=True)
-        label_str = self.tok.batch_decode(labels, group_tokens=False, skip_special_tokens=True)
-        wer = jiwer.wer(label_str, pred_str)
-        return wer
+  def accumulate(self, learn):
+      pred_logits = learn.pred.logits
+      labels = learn.yb[-1].detach().cpu().numpy()
+      pred_ids = np.argmax(pred_logits.detach().cpu().numpy(), axis=-1)
+      pred_str = self.tok.batch_decode(
+          pred_ids, group_tokens=True, skip_special_tokens=True
+      )
+      label_str = self.tok.batch_decode(
+          labels, group_tokens=False, skip_special_tokens=True
+      )
+      pred_str = [i if i != "" else "[NONE]" for i in pred_str]
+      self.pred_strs += pred_str
+      self.label_strs += label_str
+
+
+  @property
+  def value(self):
+      cer = jiwer.wer(self.pred_strs, self.label_strs)
+      return cer
 
 class CER(Metric):
-    def __init__(self, tok): self.tok = tok
-    @property
-    def name(self): return "cer"
+  def __init__(self, tok):
+      self.metric_name = "cer"
+      self.tok = tok
+      self.pred_strs, self.label_strs = [], []
 
-    def __call__(self, pred, labels):
-        pred_logits = pred.logits
-        pred_ids = np.argmax(pred_logits.detach().cpu().numpy(), axis=-1)
-        pred_str = self.tok.batch_decode(pred_ids, group_tokens=True, skip_special_tokens=True)
-        label_str = self.tok.batch_decode(labels, group_tokens=False, skip_special_tokens=True)
-        cer = jiwer.cer(label_str, pred_str)
-        return cer
+  @property
+  def name(self):
+      return "cer"
+
+  def accumulate(self, learn):
+      pred_logits = learn.pred.logits
+      labels = learn.yb[-1].detach().cpu().numpy()
+      pred_ids = np.argmax(pred_logits.detach().cpu().numpy(), axis=-1)
+      pred_str = self.tok.batch_decode(
+          pred_ids, group_tokens=True, skip_special_tokens=True
+      )
+      label_str = self.tok.batch_decode(
+          labels, group_tokens=False, skip_special_tokens=True
+      )
+      pred_str = [i if i != "" else "[NONE]" for i in pred_str]
+
+      self.pred_strs += pred_str
+      self.label_strs += label_str
+
+
+  @property
+  def value(self):
+      cer = jiwer.cer(self.pred_strs, self.label_strs)
+      return cer
