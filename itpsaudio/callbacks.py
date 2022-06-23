45ef2f2bffe186f7432a317864124e5c41ea45b0
Jiyan Jonas Schneider <jiyan.schneider@keio.jp>
2022-06-23 13:33:47 +0900

    formatting

--- itpsaudio/callbacks.py
+++ itpsaudio/callbacks.py
@@ -9,6 +9,7 @@ from fastai.data.all import store_attr, to_float, join_path_file
 
 import os
 
+
 class MixedPrecisionTransformers(MixedPrecision):
     def after_pred(self):
         if self.pred.logits.dtype == torch.float16:
@@ -31,9 +32,12 @@ class SeePreds(TensorBoardBaseCallback):
         decoded_preds = []
         decoded_ys = []
         for i, b in enumerate(iter(self.dls.valid)):
-            if len(b) == 4: x,y = b[0], b[-1]
-            elif len(b) == 2: x,y = b[0], b[-1]
-            else: assert False, "Don't know how to See these preds"
+            if len(b) == 4:
+                x, y = b[0], b[-1]
+            elif len(b) == 2:
+                x, y = b[0], b[-1]
+            else:
+                assert False, "Don't know how to See these preds"
 
             if i < self.n_vals:
                 with torch.no_grad():
@@ -42,7 +46,10 @@ class SeePreds(TensorBoardBaseCallback):
                 decoded_ys += self.tok.batch_decode(y, group_tokens=False)
             else:
                 break
-        return list(map(lambda x: x[:500], decoded_preds)),list(map(lambda x:x[:500], decoded_ys))
+        return (
+            decoded_preds[:200],
+            decoded_ys[:200],
+        )  # If the predictions are too long, neptune bugs
 
     def _log_preds_to_path(self, p, y, path):
         self.writer.add_text(
@@ -73,6 +80,7 @@ class SeePreds(TensorBoardBaseCallback):
             for i, (p, y) in enumerate(zip(dec_preds_valid, dec_ys_valid)):
                 self._log_preds_to_path(p, y, f"valid_{i}")
 
+
 class NeptuneSaveModel(Callback):
     def __init__(self, n_epochs):
         super().__init__()
@@ -83,8 +91,15 @@ class NeptuneSaveModel(Callback):
 
     def after_epoch(self):
         if self.iter % self.n_epochs == 0:
-            _file = join_path_file(self.learn.save_model.fname, self.learn.path / self.learn.model_dir, ext=".pth")
+            _file = join_path_file(
+                self.learn.save_model.fname,
+                self.learn.path / self.learn.model_dir,
+                ext=".pth",
+            )
             if os.path.exists(_file):
                 neptune.log_artifact(str(_file))
             else:
-                l.log(l.WARNING, f"Could find {_file}, thus couldn't upload it to Neptune.")
+                l.log(
+                    l.WARNING,
+                    f"Could find {_file}, thus couldn't upload it to Neptune.",
+                )
