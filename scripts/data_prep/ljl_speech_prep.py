435e6aa7958b703e575a52d05bb454201dbd667b
jiyans <jiyan.schneider@keio.jp>
2022-06-24 22:34:09 +0900

    add audio length to ljl set

--- scripts/data_prep/ljl_speech_prep.py
+++ scripts/data_prep/ljl_speech_prep.py
@@ -1,24 +1,31 @@
 #!/usr/bin/env python3
-#!/usr/bin/env python3
-from fastai.data.all import untar_data, Path
-from fastai.data.transforms import RandomSplitter
+import os
+import shutil
+
 import pandas as pd
-import shutil, os
 from dsets.helpers.helpers import make_tarfile, train_test_split
+from fastai.data.all import Path
+from fastdownload import FastDownload
+import torchaudio
 
-LJ_SPEECH_URL_ORIG = "https://www.dropbox.com/s/cwq264n040guqhj/LJSpeech-1.1.tar.bz2?dl=1"
-OUTPATH = "/home/jjs/Dropbox/Share to iTPS AI-Team/train_data_repository/LJSpeech-1.1.tar.gz"
-FORCE_DOWNLOAD=True
+LJ_SPEECH_URL_ORIG = (
+    "https://www.dropbox.com/s/cwq264n040guqhj/LJSpeech-1.1.tar.bz2?dl=1"
+)
+DATAROOT = "/home/jjs/proj/work/itps/itps_transcription_model/scripts/data/"
+OUTPATH = Path(DATAROOT) / "LJSpeech-1.1.tar.gz"
+FORCE_DOWNLOAD = False
 
-def get_ljl_data_init(force_download=False):
-    p = untar_data(LJ_SPEECH_URL_ORIG, force_download=force_download)
+
+def get_ljl_data_init(base, force_download=False):
+    d = FastDownload(base=base)
+    p = d.get(LJ_SPEECH_URL_ORIG, force=force_download)
     df = pd.read_csv(p / "metadata.csv", sep="|", header=None)
     df[0] = df[0].apply(lambda x: p / "wavs" / (x + ".wav"))
     df = df.rename({0: "filename", 1: "text (numbers)", 2: "text"}, axis="columns")
     return p, df
 
 
-p, df = get_ljl_data_init(force_download=FORCE_DOWNLOAD)
+p, df = get_ljl_data_init(Path(DATAROOT), force_download=FORCE_DOWNLOAD)
 df = train_test_split(df)
 
 os.mkdir(p / "wavs" / "train")
@@ -34,12 +41,21 @@ for i, r in df.iterrows():
         dest = r["filename"].parent / "train" / r["filename"].name
         shutil.move(src, dest)
 
+
+def get_frames_sr(f):
+    t, sr = torchaudio.load(f)
+    no_frames = len(t.squeeze())
+    return pd.Series([no_frames, sr])
+
+df[["no_frames", "sr"]] = df["filename"].apply(get_frames_sr)
+df["audio_length"] = df["no_frames"] / df["sr"]
+
 df["filename"] = df.apply(
     lambda r: Path("wavs") / "test" / r["filename"].name
     if r["test"]
-    else Path("wavs") / "train" / r["filename"].name
-    ,axis=1
+    else Path("wavs") / "train" / r["filename"].name,
+    axis=1,
 )
 
-df.to_csv( p / "metadata.csv")
+df.to_csv(p / "metadata.csv")
 make_tarfile(OUTPATH, p)
