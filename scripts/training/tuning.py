b93c4b03d5ddd9a3a5aa6cf5e6012828d3144196
jiyans <jiyan.schneider@keio.jp>
2022-07-06 16:34:27 +0900

    call log_predictions correctly

--- scripts/training/tuning.py
+++ scripts/training/tuning.py
@@ -561,9 +561,10 @@ def log_preds_for_df(learn, dls, df, title):
         logger.info("Got all the predictions for itps test")
         valid_loss, perplexity, cer, wer = learn.validate(dl=dl)
     else:
-        _, _, _, dl = log_predictions(learn, dls, df)
         logger.info("Got all the predictions for itps test")
         valid_loss, perplexity, cer, wer = learn.validate()
+        _, _, _, dl = log_predictions(learn, dls, cer)
+
     logger.info(
         f"{wandb.run.name}: Got evaluation for {title}: WER: {wer}, test CER: {cer}"
     )
